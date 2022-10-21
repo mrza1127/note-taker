3a29408c4cb4cf81bbaa98100db50da393731f33
isaias <104476799+mrza1127@users.noreply.github.com>
2022-10-21 14:46:27 -0500

    creating id's for notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,14 +1,14 @@
 const router = require("express").Router()
 const { Router } = require("express")
 const notes = require("../../db/db.json")
-const createNewNote = require("../../lib/note")
+const { createNewNote, assignUniqueId } = require("../../lib/note")
 
 router.get("/notes", (req, res) => {
     res.json(notes)
 })
 
 router.post("/notes", (req, res) => {
-    const note = createNewNote(req.body, notes)
+    const note = createNewNote(assignUniqueId(req, notes), notes)
     res.json(note)
 
 })
